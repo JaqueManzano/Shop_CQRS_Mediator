18cc65723f0efac768d64266ba13801d66c5f811
Jaqueline Manzano <Jaque_manzanos@hotmail.com>
2024-03-14 22:12:32 -0300

    Aula de CQRS e Mediator do Balta

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+using Shop.Domain.Handlers;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [ApiController]
+    [Route("v1/customers")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public Task<CreateCustomerResponse> Create([FromServices] IMediator mediator, [FromBody]CreateCustomerRequest command)
+        {
+            return mediator.Send(command);
+        }
+    }
+}
---
+++ Domain/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Handlers
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
+    {
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+            // Verifica se o cliente já está cadastrado
+            // Valida os dados
+            // Insere o cliente
+            // Envia e-mail de boas vindas
+
+            var result = new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "André Baltiei",
+                Email = "andre@balta.io",
+                Date = DateTime.Now
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
